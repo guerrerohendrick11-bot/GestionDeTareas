8a31ab664335cb8fb27eebffc83c0f06f2ea0988
Hendrick guerrero <guerrerohendrick11@gmail.com>
2026-02-22 20:25:39 -0600

    Actualizamos el progran del frontend para que la pagina tareas se muestre

--- GestionDeTareasBlazor/Program.cs
+++ GestionDeTareasBlazor/Program.cs
@@ -3,21 +3,33 @@ using GestionDeTareasBlazor.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-builder.Services.AddHttpClient("Api", client =>
-{
-   
-    client.BaseAddress = new Uri("https://gestiondetareas-2.onrender.com/");
-});
 
+builder.Services.AddScoped(sp =>
+{
+    var env = builder.Environment;
+
+    if (env.IsDevelopment())
+    {
+        return new HttpClient
+        {
+            BaseAddress = new Uri("http://localhost:8080")
+        };
+    }
+    else
+    {
+        return new HttpClient
+        {
+            BaseAddress = new Uri("https://gestiondetareas-2.onrender.com")
+        };
+    }
+});
 
 builder.Services.AddScoped<TareaApiService>();
 
 var app = builder.Build();
-
 
 if (!app.Environment.IsDevelopment())
 {
@@ -27,13 +39,9 @@ if (!app.Environment.IsDevelopment())
 
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
-
-
 app.UseStaticFiles();
 app.UseAntiforgery();
-
 app.MapStaticAssets();
-
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
