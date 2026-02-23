a2427cbead1d9200448457a8b0ce43a9e4af5998
Hendrick guerrero <guerrerohendrick11@gmail.com>
2026-02-22 18:06:07 -0600

    Progran del frontend y backend actualizados

--- GestionDeTareasBlazor/Program.cs
+++ GestionDeTareasBlazor/Program.cs
@@ -3,36 +3,38 @@ using GestionDeTareasBlazor.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
 builder.Services.AddHttpClient("Api", client =>
 {
-    client.BaseAddress = new Uri("https://localhost:7281");
+   
+    client.BaseAddress = new Uri("https://gestiondetareas-2.onrender.com/");
 });
+
 
 builder.Services.AddScoped<TareaApiService>();
 
-
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
+
+app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapStaticAssets();
+
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
